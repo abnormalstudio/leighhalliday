70ba117910cc5271fefffe2031d615b12a9ca02c
Leigh Halliday <leighhalliday@gmail.com>
2019-09-29 10:41:50 -0400

    Adding link to Marians website

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -40,10 +40,10 @@ const About = () => (
           </div>
           <p>
             My name is Leigh Halliday! I'm currently based out of Toronto (well,
-            Rockwood!), Canada, where I live with my wonderful wife Marian
-            Serna. I am a developer at{" "}
-            <a href="https://www.flipgive.com">FlipGive</a>. I produce React and
-            JavaScript tutorials on{" "}
+            Rockwood!), Canada, where I live with my wonderful wife{" "}
+            <a href="https://www.privacyforstartups.com/about">Marian Serna</a>.
+            I am a developer at <a href="https://www.flipgive.com">FlipGive</a>.
+            I produce React and JavaScript tutorials on{" "}
             <a href="https://www.youtube.com/leighhalliday">YouTube</a>.
           </p>
 
