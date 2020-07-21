1c38416a7619934eb175e34fbcee3cdf045a4f1a
Leigh Halliday <leighhalliday@gmail.com>
2020-07-21 12:21:44 -0400

    Fixing gatsby-browser error

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,17 +1,20 @@
-const Appsignal = require("@appsignal/javascript");
-const { ErrorBoundary } = require("@appsignal/react");
+// const React = require("react");
+// const Appsignal = require("@appsignal/javascript").default;
+// const ErrorBoundary = require("@appsignal/react").default;
 
-const appsignal = new Appsignal({
-  key: process.env.GATSBY_APPSIGNAL_KEY,
-});
+// console.log(ErrorBoundary, "appsignal");
 
-const FallbackComponent = () => <div>Uh oh! There was an error :(</div>;
+// const appsignal = new Appsignal({
+//   key: process.env.GATSBY_APPSIGNAL_KEY,
+// });
 
-exports.wrapRootElement = ({ element }) => (
-  <ErrorBoundary
-    instance={appsignal}
-    fallback={(error) => <FallbackComponent />}
-  >
-    {element}
-  </ErrorBoundary>
-);
+// const FallbackComponent = () => <div>Uh oh! There was an error :(</div>;
+
+// exports.wrapRootElement = ({ element }) => (
+//   <ErrorBoundary
+//     instance={appsignal}
+//     fallback={(error) => <FallbackComponent />}
+//   >
+//     {element}
+//   </ErrorBoundary>
+// );
